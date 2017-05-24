8080695d080fd2e035f6d94d9eb57cb50e566c6d
Javier Coppis <javier.coppis@gmial.com>
2017-05-24 11:56:19 -0300

    second commit

--- getcnn/main.go
+++ getcnn/main.go
@@ -12,7 +12,8 @@ import (
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/about", about)
-	http.HandleFunc("/reddit", getReddit)
+	http.HandleFunc("/cnn", getCNN)
+	fmt.Printf("Servidor iniciado en el puerto '%d'\n", 8080)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -20,7 +21,7 @@ func home(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Mi pagina")
 }
 
-func getReddit(w http.ResponseWriter, req *http.Request) {
+func getCNN(w http.ResponseWriter, req *http.Request) {
 	items, err := goCNNTop10.Get()
 	if err != nil {
 		log.Fatal(err)
