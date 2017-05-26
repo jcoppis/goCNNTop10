af3c3460c2a6e4f075646146d88d5349c8548d32
Javier Coppis <javier.coppis@gmial.com>
2017-05-26 10:24:01 -0300

    implementado envio JSON

--- getcnn/main.go
+++ getcnn/main.go
@@ -6,16 +6,25 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/jcoppis/goCNNTop10"
 )
+
+func infoServer(puerto int, endPoints []string) {
+	fmt.Printf("Servidor iniciado en http://localhost:%d\n", puerto)
+	fmt.Print("Puede conectarse a los siguientes endPoints:")
+	for _, endPoint := range endPoints {
+		fmt.Printf(" /%s", endPoint)
+	}
+}
 
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/about", about)
 	http.HandleFunc("/cnn", getCNN)
 	http.HandleFunc("/json", getJSON)
-	fmt.Printf("Servidor iniciado en el puerto '%d'\n", 8080)
+	infoServer(8080, strings.Fields("about cnn json"))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -40,6 +49,7 @@ func about(w http.ResponseWriter, req *http.Request) {
 }
 
 func getJSON(w http.ResponseWriter, r *http.Request) {
-	jsonStr := []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-	json.NewEncoder(w).Encode(jsonStr)
+	item := goCNNTop10.Item{Title: "Mi JSON", URL: "http://json.org", Description: "Ejemplo json"}
+	//jsonStr := []byte(`{"title":"Buy cheese and bread for breakfast.", "subtitle":"hola"}`)
+	json.NewEncoder(w).Encode(item)
 }
